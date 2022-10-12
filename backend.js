5239f0f83c7bd519ce45456e9e6e78625dd17f70
Lucas Abdo <luc.abdo01@gmail.com>
2022-10-11 22:51:18 -0700

    added mongoDB

--- backend.js
+++ backend.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
-const port = 5000;
 const cors = require('cors');
 
+const app = express();
+const port = 5000;
+
+const userServices = require('./models/user-services');
+
+/*
 const users = { 
     users_list :
     [
@@ -33,28 +37,29 @@ const users = {
        }
     ]
  }
+ */
 
 app.use(cors());
 app.use(express.json());
 
 
 //DELETE a user by given ID
-app.delete('/users/:id', (req, res) => {    
+app.delete('/users/:id', async (req, res) => {    
     const id = req.params['id']; //or req.params.id
-    let uLength = users['users_list'].length;
-    let result = deleteUser(id);
+    //let uLength = users['users_list'].length;
+    let result = await userServices.deleteUserById(id);
 
-    if (result.length === uLength)
-        res.status(404).send('Could not delete user');
-    else {
+    if (result)
         res.status(204).end();
-    }
+    else 
+        res.status(404).send('Could not delete user');
 });
 
 function deleteUser(id){
     return users['users_list'] = users['users_list'].filter( (user) => user['id'] !== id);
 }
 
+/*
 //POST new user to list of users
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
@@ -62,6 +67,17 @@ app.post('/users', (req, res) => {
         userToAdd['id'] = randomId();
     addUser(userToAdd);
     res.status(201).send(userToAdd);
+});
+*/
+
+//POST new user to list of users
+app.post('/users', async (req, res) => {
+    const user = req.body;
+    const savedUser = await userServices.addUser(user);
+    if (user)
+        res.status(201).send(user);
+    else
+        res.status(500).end();
 });
 
 function randomId(){
@@ -78,6 +94,7 @@ function addUser(user){
     users['users_list'].push(user);
 }
 
+/*
 //GET response to find user by name
 app.get('/users', (req, res) => {
     const name = req.query.name;
@@ -100,6 +117,35 @@ app.get('/users', (req, res) => {
         res.send(users);
     }
 });
+*/
+
+//GET response to find user by name
+app.get('/users', async (req, res) => {
+    const name = req.query.name;
+    const job = req.query.job;
+
+    //GET by name & job
+    try{
+        let result = await userServices.getUsers(name, job);
+        result = {users_list: result};
+        res.send(result);
+    } catch (error){
+        console.log(error);
+        res.status(500).send('An error occured in the server.');
+    }
+
+    /*
+    //GET by name
+    else if (name != undefined){
+        let result = await userServices.getUsers(name, job);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
+    */
+});
 
 const findUserByNameJob = (name, job) => { 
     return users['users_list'].filter( (user) => user['name'] === name).filter((user) => user['job'] === job); 
@@ -109,6 +155,7 @@ const findUserByName = (name) => {
     return users['users_list'].filter( (user) => user['name'] === name); 
 }
 
+/*
 //GET response to find user by id
 app.get('/users/:id', (req, res) => {
     const id = req.params['id']; //or req.params.id
@@ -120,6 +167,20 @@ app.get('/users/:id', (req, res) => {
         res.send(result);
     }
 });
+*/
+
+//GET response to find user by id
+app.get('/users/:id', async (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = await userServices.findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
+
 
 function findUserById(id) {
     return users['users_list'].find( (user) => user['id'] === id); // or line below
